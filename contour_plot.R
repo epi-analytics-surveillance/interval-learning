720047808eedccb5a733370039e48c71b27fdf47
zimow2 <Suzie979797@gmail.com>
2025-06-20 13:12:51 +1000

    Add files via upload

--- contour_plot.R
+++ contour_plot.R
@@ -17,6 +17,7 @@ setwd("~/Documents/Biostatistics/project/R code")
 library(tidyverse)
 library(akima)
 library(ggplot2)
+library(patchwork)
 
 # Simulate case counts function
 simulate_case_counts <- function(R_t, W_s, I_0 = 10) {
@@ -88,7 +89,7 @@ log_likelihood <- function(I, R_t, W_s) {
 }
 
 ################################################################################
-# Generate GI and Rt
+# Generate GI and Rt function
 sim_GI_Rt <- function(GI_mean, GI_sd, Rt_mean, Rt_sd, Rt_length) {
   
   # Log-normal parameters for GI
@@ -118,25 +119,20 @@ sim_GI_Rt <- function(GI_mean, GI_sd, Rt_mean, Rt_sd, Rt_length) {
   ))
 }
 
-# data for 20 days
-data <- sim_GI_Rt(GI_mean = 4.5, GI_sd = 1,  
-                  Rt_mean = 2.75, Rt_sd = 0.001, Rt_length = 20)
-
-# Simulate case count data
-case_count <- simulate_case_counts(data$Rt[[1]], data$Ws[[1]], I_0 = 10)
 ################################################################################
-# Generate log-likelihood data set
-ll_plot_data <- function(N = 100000, case_count_days = 50, I = case_count){
+# Generate log-likelihood data set function
+ll_plot_data <- function(N = 100000, case_count_days = 50, I = case_count,
+                         Rt_lower = 0.5, Rt_upper = 5, Rt_sd = 0.001){
   # N: the number of observations of mean, sd, and log-likelihood
   # case_count_days: the length of the simulated case count data
   # I: Simulated series of case count
  
    # generate a series of random number as mean and sd
   data <- tibble(
-    GI_mean = runif(N, min = 2, max = 7), # GI mean range from 2-7 days
-    GI_sd = runif(N, min = 0.5, max = 1.5), # GI sd range from 1-3 days
-    Rt_mean = runif(N, min = 0.5, max = 5), # Rt mean range from 0.5-5
-    Rt_sd = rep(0.001, N), # Rt sd set to be 0.001
+    GI_mean = runif(N, min = 3, max = 7), # GI mean range
+    GI_sd = rep(1, N), # GI sd set to be 1
+    Rt_mean = runif(N, min = Rt_lower, max = Rt_upper), # Rt mean range
+    Rt_sd = rep(Rt_sd, N), # Rt sd set to be 0.001
     Rt_length = rep(case_count_days, N)
   )
   
@@ -151,22 +147,46 @@ ll_plot_data <- function(N = 100000, case_count_days = 50, I = case_count){
   
   # Calculate the log-likelihood
   for (i in 1:N) {
-    final_data$log_like[i] <- log_likelihood(I, final_data$Rt[[i]], final_data$Ws[[i]])
+    final_data$ll[i] <- log_likelihood(I, final_data$Rt[[i]], final_data$Ws[[i]])
   }
   
   return(final_data)
 }
 
-data <- ll_plot_data(N = 100000, case_count_days = 50, I = case_count)
-View(data) # check the parameters, Rt, Ws, and log-likelihood
+################################################################################
+# Single plot
+# Set parameters
+parameters <- data.frame(
+  GI_mean = 5,
+  GI_sd = 1,
+  Rt_mean = 2,
+  Rt_sd = 0.001,
+  Rt_lower = 0.5,
+  Rt_upper = 3.5,
+  days = 20
+  )
+
+# Simulate Rt and Ws
+data <- sim_GI_Rt(GI_mean = parameters$GI_mean, GI_sd = parameters$GI_sd,  
+                  Rt_mean = parameters$Rt_mean, Rt_sd = parameters$Rt_sd, 
+                  Rt_length = parameters$days)
+
+# Simulate case count data
+case_count <- simulate_case_counts(data$Rt[[1]], data$Ws[[1]], I_0 = 10)
+
+# Calculate log-likelihood
+data <- ll_plot_data(N = 100000, case_count_days = parameters$days, I = case_count,
+                     Rt_lower = parameters$Rt_lower, Rt_upper = parameters$Rt_upper, 
+                     Rt_sd = parameters$Rt_sd)
+data <- data[data$ll  >= -100,] # zoom in for log-likelihood < 100
 
 # Generate regular grid data for contour plot
 reg_data <- interp(
   x = data$GI_mean,   
   y = data$Rt_mean,   
-  z = data$log_like,  
-  xo = seq(min(data$GI_mean), max(data$GI_mean), length = 50),  # X grid points
-  yo = seq(min(data$Rt_mean), max(data$Rt_mean), length = 50),  # Y grid points
+  z = data$ll,  
+  xo = seq(min(data$GI_mean), max(data$GI_mean), length = 100),  # X grid points
+  yo = seq(min(data$Rt_mean), max(data$Rt_mean), length = 100),  # Y grid points
   linear = TRUE          # Use linear interpolation
 )
 
@@ -177,7 +197,6 @@ reg_df <- data.frame(
   log_like = as.vector(reg_data$z)
 )
 
-
 # Generate contour plot
 ggplot(reg_df, aes(x = GI_mean, y = Rt_mean, z = log_like)) +
   geom_contour_filled() +  # Filled contours
@@ -185,17 +204,94 @@ ggplot(reg_df, aes(x = GI_mean, y = Rt_mean, z = log_like)) +
   scale_fill_viridis_d(name = "Log-Likelihood") +   # Color scale
   labs(title = "Log-Likelihood Contour Plot",
     x = "Generation Interval Mean (days)",
-    y = "Reproduction Number (R)") +
-  geom_point(aes(x = 4.5, y = 2.75), size = 5, shape = 10) +
+    y = "Reproduction Number (R)",
+    caption = paste0("Simulated Case Count: ", toString(case_count))) +
+  theme(plot.caption = element_text(hjust = 0)) +
+  scale_x_continuous(limits = c(2, 7)) +
+  geom_point(data=parameters, aes(x = GI_mean, y = Rt_mean), 
+             size = 5, shape = 10, color = "#FF3300", inherit.aes = FALSE) +
+  geom_text(data=parameters, aes(x = GI_mean, y = Rt_mean), 
+            label=paste0("(",parameters$GI_mean,",",parameters$Rt_mean,")"), 
+            nudge_y = 0.13, color = "#FF3300", inherit.aes = FALSE) +
   theme_minimal()
 
+################################################################################
+# Multiple plots
+# Create an empty list for multiple plots
+plot_list <- list()
 
-# Alternative: use filled.contour
-filled.contour(x = reg_data$x,
-               y = reg_data$y,
-               z = reg_data$z,
-               xlab = "Generation Interval Mean (days)",
-               ylab = "Reproduction Number (R)",
-               main = "Log-Likelihood Contour Plot",
-               key.title = title(main = "Log-Likelihood", cex.main = 1))
+# Set parameters
+for (i in seq(1.5, 3, 0.5)){
+  # Set parameters
+  parameters <- data.frame(
+    GI_mean = 5,
+    GI_sd = 1,
+    Rt_mean = i,
+    Rt_sd = 0.001,
+    Rt_lower = 1.5,
+    Rt_upper = 3,
+    days = 20
+  )
+  
+  # Simulate Rt and Ws
+  data <- sim_GI_Rt(GI_mean = parameters$GI_mean, GI_sd = parameters$GI_sd,  
+                    Rt_mean = parameters$Rt_mean, Rt_sd = parameters$Rt_sd, 
+                    Rt_length = parameters$days)
+  
+  # Simulate case count data
+  case_count <- simulate_case_counts(data$Rt[[1]], data$Ws[[1]], I_0 = 10)
+  
+  # Calculate log-likelihood
+  data <- ll_plot_data(N = 100000, case_count_days = parameters$days, I = case_count,
+                       Rt_lower = parameters$Rt_lower, Rt_upper = parameters$Rt_upper, 
+                       Rt_sd = parameters$Rt_sd)
+  data <- data[data$ll  >= -100,] # zoom in for log-likelihood < 100
+  
+  # Generate regular grid data for contour plot
+  reg_data <- interp(
+    x = data$GI_mean,   
+    y = data$Rt_mean,   
+    z = data$ll,  
+    xo = seq(min(data$GI_mean), max(data$GI_mean), length = 50),  # X grid points
+    yo = seq(min(data$Rt_mean), max(data$Rt_mean), length = 50),  # Y grid points
+    linear = TRUE          # Use linear interpolation
+  )
+  
+  # Convert to data frame for ggplot
+  reg_df <- data.frame(
+    GI_mean = rep(reg_data$x, each = length(reg_data$y)),
+    Rt_mean = rep(reg_data$y, length(reg_data$x)),
+    log_like = as.vector(reg_data$z)
+  )
+  
+  # Generate contour plot
+  p <- ggplot(reg_df, aes(x = GI_mean, y = Rt_mean, z = log_like)) +
+    geom_contour_filled() +  # Filled contours
+    #geom_contour() +  # Add contour lines
+    scale_fill_viridis_d(name = "Log-Likelihood") +  # Color scale
+    labs(title = "Log-Likelihood Contour Plot",
+         x = "Generation Interval Mean (days)",
+         y = "Reproduction Number (R)",
+         caption = paste0("Simulated Case Count: ", toString(case_count))) +
+    theme(plot.caption = element_text(hjust = 0)) +
+    scale_x_continuous(limits = c(2, 7)) +
+    scale_y_continuous(limits = c(parameters$Rt_lower, parameters$Rt_upper)) +
+    geom_point(data=parameters, aes(x = GI_mean, y = as.numeric(Rt_mean)), 
+               size = 5, shape = 10, color = "#FF3300", inherit.aes = FALSE) +
+    geom_text(data=parameters, aes(x = GI_mean, y = as.numeric(Rt_mean)), 
+              label=paste0("(",parameters$GI_mean,",",parameters$Rt_mean,")"), 
+              nudge_y = 0.13, color = "#FF3300", inherit.aes = FALSE) +
+    theme_minimal()
+  
+  plot_list[[i*2]] <- p
+}
 
+# Combine the plots together
+plot_list <- discard(plot_list, is.null)
+combined_plot <- wrap_plots(plot_list, ncol = 2) # 2 columns
+print(combined_plot)
+
+# check GI shape and Rt length
+w <- data.frame(w = data$Ws[[2908]], i = seq(1,length(data$Ws[[2908]]),1))
+ggplot(w, aes(i, w)) + geom_line()
+length(data$Rt[[2908]])
